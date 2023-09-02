6f4071cbe7f51efc6630ba6e72883de91bf1af5a
psych0wh1z <ikeagudike@gmail.com>
2023-09-02 07:25:06 +0100

    Add custom error classes and error handlers

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+import AppError from './utils/AppError.js'
 import Campground from './models/campground.js'
+import catchAsync from './utils/catchAsync.js'
 import ejsMate from 'ejs-mate'
 import express from 'express'
 import methodOverride from 'method-override'
@@ -27,39 +29,48 @@ app.get('/', (req, res) => {
 	res.render('home')
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
 	const campgrounds = await Campground.find({})
 	res.render('campgrounds/index', { campgrounds })
-})
+}))
 
 app.get('/campgrounds/new', (req, res) => {
 	res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
 	const campground = new Campground(req.body.campground)
 	await campground.save()
 	res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id)
 	res.render('campgrounds/show', { campground })
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id)
 	res.render('campgrounds/edit', { campground })
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
 	const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground })
 	res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 	await Campground.findByIdAndDelete(req.params.id)
 	res.redirect('/campgrounds/')
+}))
+
+app.all('*', (req, res, next) => {
+	next(new AppError(404, 'PAGE NOT FOUND'))
+})
+
+app.use((err, req, res, next) => {
+	const { statusCode = 500, message = 'SOMETHING WENT WRONG' } = err
+	res.status(statusCode).send(message)
 })
 
 app.listen(3000, () => {
