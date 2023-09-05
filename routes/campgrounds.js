4f43584cafc7bf8c6d1eb765a670704b2ce2b52d
110nard0 <76947677+110nard0@users.noreply.github.com>
2023-09-05 07:51:54 +0100

    Break out routes and restructure app entry file

---
+++ routes/campgrounds.js
@@ -0,0 +1,56 @@
+import express from 'express'
+
+import AppError from '../utils/AppError.js'
+import catchAsync from '../utils/catchAsync.js'
+
+import Campground from '../models/campground.js'
+import { campgroundSchema } from '../schemas/schema.js'
+
+const router = express.Router()
+
+const validateCampground = (req, res, next) => {
+	const { error } = campgroundSchema.validate(req.body)
+	if (error) {
+		const msg = error.details.map(elem => elem.message).join(',')
+		throw new AppError(400, msg)
+	} else {
+		next()
+	}
+}
+
+router.get('/', catchAsync(async (req, res) => {
+	const campgrounds = await Campground.find({})
+	res.render('campgrounds/index', { campgrounds })
+}))
+
+router.get('/new', (req, res) => {
+	res.render('campgrounds/new')
+})
+
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+	const campground = new Campground(req.body.campground)
+	await campground.save()
+	res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+router.get('/:id', catchAsync(async (req, res) => {
+	const campground = await Campground.findById(req.params.id).populate('reviews')
+	res.render('campgrounds/show', { campground })
+}))
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+	const campground = await Campground.findById(req.params.id)
+	res.render('campgrounds/edit', { campground })
+}))
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+	const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground })
+	res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+router.delete('/:id', catchAsync(async (req, res) => {
+	await Campground.findByIdAndDelete(req.params.id)
+	res.redirect('/campgrounds')
+}))
+
+export default router
---
+++ routes/reviews.js
@@ -0,0 +1,40 @@
+import express from 'express'
+
+import AppError from '../utils/AppError.js'
+import catchAsync from '../utils/catchAsync.js'
+
+import Campground from '../models/campground.js'
+import Review from '../models/review.js'
+import { reviewSchema } from '../schemas/schema.js'
+
+const router = express.Router({ mergeParams: true })
+
+const validateReview = (req, res, next) => {
+	const { error } = reviewSchema.validate(req.body)
+	if (error) {
+		const msg = error.details.map(elem => elem.message).join(',')
+		throw new AppError(400, msg)
+	} else {
+		next()
+	}
+}
+
+router.post('/', validateReview, catchAsync(async (req, res) => {
+	const { id } = req.params
+	const campground = await Campground.findById(id)
+	const review = new Review(req.body.review)
+	review.camp = campground._id
+	campground.reviews.push(review)
+	await review.save()
+	await campground.save()
+	res.redirect(`/campgrounds/${id}`)
+}))
+
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+	const { id, reviewId } = req.params
+	await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+	await Review.findByIdAndDelete(reviewId)
+	res.redirect(`/campgrounds/${id}`)
+}))
+
+export default router
